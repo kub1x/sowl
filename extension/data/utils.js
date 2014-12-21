35d20f12e8c4dbff395155ac0d9a6ef30b937c7e
kub1x <j.podlaha@gmail.com>
2014-12-21 05:07:38 +0100

    resolve on Object.prototype was f*king up the Object.prototype.keys() and thus the for(i in obj) moved into closure, will be used in String.prototype.format() only.

--- extension/data/utils.js
+++ extension/data/utils.js
@@ -1,42 +1,42 @@
-if (!Object.prototype.resolve) {
-  /**
-   * usage { a: { b: ['neco'] } }.resolve("a.b.0"); // returns 'neco'
-   * TODO handle ""s and ''s to allow period (the '.' char) in names. 
-   * TODO handle []s to allow internal [] notation like o.resolve('a.b[0]') maybe. 
-   */
-  Object.prototype.resolve = function(path) {
-    var p = path.split('.'), 
-        o = this, l = p.length, i;
-    try {
-      for (i=0; i<l; ++i) {
-        o = o[p[i]];
-      }; 
-      return o;
-    } catch (e) {
-      return undefined;
-    }
-  };
-};
-
 if (!String.prototype.format) {
-  /**
-   * NOTE This method determines the "type" of matching according to first parameter
-   *      if you do .format(["a"], "b"), only the array in the first attribute will be 
-   *      used, the rest will be ignored. 
-   *
-   * usage "{0} world! {1}".format("Hello", "Howdy?");   // prints "Hello world! Howdy?"
-   * usage "{0} world! {1}".format(["Hello", "Howdy?"]); // prints "Hello world! Howdy?" too
-   * usage "{first} world! {second}".format({first: "Hello", second: "Howdy?}); // prints the same
-   */
-  String.prototype.format = function(first) {
-    var args = (first instanceof Array ? first :
-               (first instanceof Object ? first :
-                arguments));
-    return this.replace(/{([^\}]+)}/g, function(match, path) { 
-      var res = Object.prototype.resolve.call(args, path);
-      return typeof res != 'undefined' ? res : match ;
-    });
-  };
+  (function() {
+    /**
+     * usage { a: { b: ['neco'] } }.resolve("a.b.0"); // returns 'neco'
+     * TODO handle ""s and ''s to allow period (the '.' char) in names. 
+     * TODO handle []s to allow internal [] notation like o.resolve('a.b[0]') maybe. 
+     */
+    function resolve(path) {
+      var p = path.split('.'), 
+          o = this, l = p.length, i;
+      try {
+        for (i=0; i<l; ++i) {
+          o = o[p[i]];
+        }; 
+        return o;
+      } catch (e) {
+        return undefined;
+      }
+    };
+
+    /**
+     * NOTE This method determines the "type" of matching according to first parameter
+     *      if you do .format(["a"], "b"), only the array in the first attribute will be 
+     *      used, the rest will be ignored. 
+     *
+     * usage "{0} world! {1}".format("Hello", "Howdy?");   // prints "Hello world! Howdy?"
+     * usage "{0} world! {1}".format(["Hello", "Howdy?"]); // prints "Hello world! Howdy?" too
+     * usage "{first} world! {second}".format({first: "Hello", second: "Howdy?}); // prints the same
+     */
+    String.prototype.format = function(first) {
+      var args = (first instanceof Array ? first :
+                 (first instanceof Object ? first :
+                  arguments));
+      return this.replace(/{([^\}]+)}/g, function(match, path) { 
+        var res = resolve.call(args, path);
+        return typeof res != 'undefined' ? res : match ;
+      });
+    };
+  })();
 }; 
 
 
